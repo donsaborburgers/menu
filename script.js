72377768e1608c8ec87d9251c750290f67f916aa
ABordon <andresbordoncardozo@gmail.com>
2024-10-15 20:51:00 -0300

    actualizando imagenes

--- script.js
+++ script.js
@@ -1,50 +1,42 @@
 let pedido = [];
 
-function addItem(item) {
-    pedido.push(item);
-    actualizarPedido();
-    mostrarNotificacion();
+function addItem(item, cantidad) {
+    if (cantidad > 0) {
+        pedido.push({ item: item, cantidad: parseInt(cantidad) });
+        actualizarPedido();
+        mostrarNotificacion();
+    } else {
+        alert("Por favor, selecciona una cantidad válida.");
+    }
+}
+
+function actualizarPedido() {
+    const pedidoDiv = document.getElementById('pedido');
+    pedidoDiv.innerHTML = '';
+    pedido.forEach((pedidoItem) => {
+        const itemPedido = document.createElement('p');
+        itemPedido.textContent = `${pedidoItem.cantidad}x ${pedidoItem.item}`;
+        pedidoDiv.appendChild(itemPedido);
+    });
 }
 
 function mostrarNotificacion() {
     const notification = document.getElementById('notification');
     notification.classList.remove('hidden');
     notification.classList.add('show');
-
-    // Oculta la notificación después de 2 segundos
     setTimeout(() => {
         notification.classList.remove('show');
         notification.classList.add('hidden');
     }, 2000);
 }
 
-
-function actualizarPedido() {
-    const pedidoDiv = document.getElementById('pedido');
-    pedidoDiv.innerHTML = ''; // Limpiar contenido previo
-
-    if (pedido.length === 0) {
-        pedidoDiv.innerHTML = '<p>Aún no has agregado nada al pedido.</p>';
-    } else {
-        pedido.forEach((item, index) => {
-            pedidoDiv.innerHTML += `<p>${index + 1}. ${item}</p>`;
-        });
-    }
-}
-
 function enviarPedido() {
-    if (pedido.length === 0) {
-        alert('No has seleccionado ningún producto.');
-        return;
-    }
-
-    let mensaje = 'Este es mi pedido: \n';
-    pedido.forEach((item, index) => {
-        mensaje += `${index + 1}. ${item}\n`;
+    let mensajePedido = 'Pedido:\n';
+    pedido.forEach(p => {
+        mensajePedido += `${p.cantidad}x ${p.item}\n`;
     });
-
-    let telefono = '595973547459'; // Reemplaza con el número al que quieres enviar el pedido
-    let urlWhatsApp = `https://wa.me/${telefono}?text=${encodeURIComponent(mensaje)}`;
-    
-    window.open(urlWhatsApp, '_blank');
+    const mensajeWhatsApp = encodeURIComponent(mensajePedido);
+    const numeroWhatsApp = "123456789"; // Reemplaza con tu número de WhatsApp
+    const url = `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`;
+    window.open(url, '_blank');
 }
